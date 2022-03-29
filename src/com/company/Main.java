8fe0a250194507c7dcb889e0871bbc59b4b91db1
dantist987 <adik.isak2412@gmail.com>
2022-03-29 20:41:42 +0600

    createObject

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Toyota toyota = new Toyota(180, "Harrier", "Crossover");
-        Nissan nissan = new Nissan(220, "Skyline", "4WD");
-        Honda honda = new Honda(200, "Accord", 2.4);
+
+        Car toyota = createObject("Toyota");
+        Car nissan = createObject("Nissan");
+        Car honda = createObject("Honda");
 
         toyota.print();
         System.out.println("--------------");
         nissan.print();
         System.out.println("--------------");
         honda.print();
+    }
+
+    private static Car createObject(String className) {
+        switch (className) {
+            case "Toyota":
+                return new Toyota(180, "Harrier", "Crossover");
+
+            case "Nissan":
+                return new Nissan(220, "Skyline", "4WD");
+
+            default:
+                return new Honda(200, "Accord", 2.4);
+
+
+        }
     }
 }
